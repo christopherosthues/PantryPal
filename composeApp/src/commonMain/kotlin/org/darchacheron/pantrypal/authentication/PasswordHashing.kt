717042cf79e71d39d62b9058fbb77b24201e00ac
Christopher Osthues <christopher.osthues@gmail.com>
2026-03-21 19:02:58 +0100

    refactor: update password hashing and verification logic
    - Updated `hashPassword` to accept and return nullable strings.
    - Added a TODO comment to implement secure hashing algorithms like BCrypt or Argon2.
    - Modified `verifyPassword` to compare the result of `hashPassword` against the stored hash.

--- composeApp/src/commonMain/kotlin/org/darchacheron/pantrypal/authentication/PasswordHashing.kt
+++ composeApp/src/commonMain/kotlin/org/darchacheron/pantrypal/authentication/PasswordHashing.kt
@@ -1,10 +1,11 @@
 package org.darchacheron.pantrypal.authentication
 
-fun hashPassword(password: String): String {
+fun hashPassword(password: String?): String? {
+    // TODO: Use something like BCrypt or Argon2
     return password
 }
 
 fun verifyPassword(password: String?, hashedPassword: String?): Boolean {
-    return password == hashedPassword
+    return hashPassword(password) == hashedPassword
 }
 
